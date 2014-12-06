a0d213e24d90bfe95342efd57cf560ae2c1996e1
Teddy Wing <git@teddywing.com>
2014-12-06 13:45:27 -0500

    git-checkout-store: Create an rc file if it doesn't exist
    Make a `.git-checkout-historyrc` file in the current user's home
    directory if the file doesn't exist.
    
    Resources:
    - [Path to home
      directory](http://stackoverflow.com/questions/7922270/obtain-users-home-directory)
    - [Cross-compilation version](https://github.com/mitchellh/go-homedir)

--- git-checkout-store/main.go
+++ git-checkout-store/main.go
@@ -3,13 +3,35 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"os/user"
 )
+
+func getHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir
+}
+
+func OpenRCFile() {
+	filename := ".git-checkout-historyrc"
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		_, err := os.Create(getHomeDir() + "/" + filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
 
 func main() {
 	args := os.Args[1:]
 
+	OpenRCFile()
+	
 	if len(args) > 0 {
 		cmd := exec.Command("git", "checkout", args[0])
 		var out bytes.Buffer
