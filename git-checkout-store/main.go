5cf065714b672445ff4f28649f8cb2ea12c2f17c
Teddy Wing <git@teddywing.com>
2014-12-06 11:45:30 -0500

    git-checkout-store: Run `git checkout` with argument
    If an argument is passed in, run `git checkout` using that first
    argument to the program.
    
    Currently doesn't accept flags from git-checkout or send output from
    git-checkout. Ideally both of those would be supported.

--- git-checkout-store/main.go
+++ git-checkout-store/main.go
@@ -3,11 +3,19 @@ package main
 
 import (
 	"os"
+	"os/exec"
 	"fmt"
 )
 
 
 func main() {
 	args := os.Args[1:]
-	fmt.Println(args)
+	
+	if len(args) > 0 {
+		cmd := exec.Command("git", "checkout", args[0])
+		err := cmd.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+		}
+	}
 }
