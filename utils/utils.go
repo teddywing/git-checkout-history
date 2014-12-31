7d6ab76e8c5b382aff8a0cb7812c904f6dda9272
Teddy Wing <git@teddywing.com>
2014-12-31 12:50:30 -0500

    utils.go: Add function to get current git repo path
    Query git to get the current repo path. I plan on using that string as
    the key for branches.
    
    This will allow us to only store and query checkout history for the
    current repository.
    
    TODO: get rid of trailing newline

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,14 @@
 package utils
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/user"
+	"regexp"
+	"strconv"
 
 	"gopkg.in/yaml.v2"
 )
@@ -21,6 +25,37 @@ func getHomeDir() string {
 		log.Fatal(err)
 	}
 	return usr.HomeDir
+}
+
+func currentGitDir() string {
+	// Check git version
+	// If below 1.7.0 then exit 1. `--show-toplevel` is not available in prior
+	// versions.
+	// /git version (\d+\.\d+).*/
+	// Run `git rev-parse --show-toplevel`
+	// Return output from the command
+	cmd := exec.Command("git", "--version")
+	var version_string bytes.Buffer
+	cmd.Stdout = &version_string
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	
+	r, _ := regexp.Compile(`git version (\d+\.\d+).*`)
+	matches := r.FindStringSubmatch(version_string.String())
+	
+	version_number, _ := strconv.ParseFloat(matches[1], 64)
+	if version_number >= 1.7 {
+		cmd = exec.Command("git", "rev-parse", "--show-toplevel")
+		var git_directory bytes.Buffer
+		cmd.Stdout = &git_directory
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+		return git_directory.String()
+	}
+	
+	return ""
 }
 
 func OpenHistoryFile() (f *os.File, err error) {
